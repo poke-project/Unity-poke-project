223c75d223757829bc0e4fa39a986a8d51f53b8c
fperret <ptidinono@gmail.com>
2016-07-19 12:58:46 +0200

    fight scene UI health management

---
+++ PokeProject/Assets/Scripts/FightPokemonInfoUI.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FightPokemonInfoUI : MonoBehaviour {
+
+    private Image status;
+    private Image barSprite;
+    private Slider hpBar;
+
+    [SerializeField]
+    private Sprite lowHp;
+    [SerializeField]
+    private Sprite mediumHp;
+    [SerializeField]
+    private Sprite highHp;
+
+    private int currentHp;
+
+    void Awake()
+    {
+        status = transform.Find("Status").GetComponent<Image>();
+        hpBar = transform.Find("Hp Bar").GetComponent<Slider>();
+        barSprite = hpBar.transform.Find("HP").GetComponent<Image>();
+
+        currentHp = 100;
+    }
+
+	void Start ()
+    {
+        hpBar.maxValue = currentHp;
+	}
+	
+	void Update ()
+    {
+	    // Remove after testing
+        if (Input.GetKeyDown(KeyCode.Space) && currentHp >= 10)
+        {
+            currentHp -= 10;
+        }
+
+        hpBar.value = hpBar.maxValue - currentHp;
+        if (currentHp < (hpBar.maxValue / 5))
+        {
+            barSprite.sprite = lowHp;
+        }
+        else if (currentHp < (hpBar.maxValue / 2))
+        {
+            barSprite.sprite = mediumHp;
+        }
+        else
+        {
+            barSprite.sprite = highHp;
+        }
+	}
+}
---
+++ PokeProject/Assets/Scripts/FightSceneManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class FightSceneManager : MonoBehaviour {
+
+    public static FightSceneManager instance;
+
+    [SerializeField]
+    public Dictionary<string, Sprite> numbers;
+
+    public Sprite[] sprites;
+
+    void Awake()
+    {
+        instance = this;
+
+        loadNumbersInDic();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void loadNumbersInDic()
+    {
+        numbers = new Dictionary<string, Sprite>();
+        Sprite[] tmp = Resources.LoadAll<Sprite>("Sprites/Numbers");
+        sprites = tmp;
+        foreach (Sprite sprite in tmp)
+        {
+            numbers.Add(sprite.name, sprite);
+        }
+    }
+}
